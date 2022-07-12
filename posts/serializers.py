564fa76297ac3c7093326e5576dde828cf7ac617
adiglase <adiglase6@gmail.com>
2022-07-12 15:58:20 +0700

    to output user id in post serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -5,6 +5,7 @@ from posts.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    user_id = serializers.SerializerMethodField()
     user_image = serializers.SerializerMethodField()
     user_full_name = serializers.SerializerMethodField()
     user_username = serializers.SerializerMethodField()
@@ -14,7 +15,7 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ['description', 'id', 'image', 'created_at', 'total_likes', 'is_liked', 'user_image', 'user_full_name', 'user_username', 'total_comments']
+        fields = ['description', 'id', 'image', 'created_at', 'total_likes', 'is_liked', 'user_id', 'user_image', 'user_full_name', 'user_username', 'total_comments']
         read_only_fields = (
             "created_by",
             "created_at"
@@ -29,6 +30,9 @@ class PostSerializer(serializers.ModelSerializer):
 
     def get_total_comments(self, obj):
         return obj.comments.count()
+
+    def get_user_id(self, obj):
+        return obj.created_by.id
 
     def get_user_image(self, obj):
         return settings.HOME_URL + obj.created_by.image.url if obj.created_by.image else None
