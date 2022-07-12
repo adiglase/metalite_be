d080a420f1bdd83598f5ea26a9c07b45e9cbe8b3
adiglase <adiglase6@gmail.com>
2022-07-13 00:12:11 +0700

    implement user suggestion list api

--- users/views.py
+++ users/views.py
@@ -1,5 +1,8 @@
+from multiprocessing import context
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from users.serializers import CustomUserSerializer
 
@@ -10,3 +13,14 @@ class CustomUserViewSet(viewsets.ModelViewSet):
     serializer_class = CustomUserSerializer
     queryset = User.objects.filter(is_staff=False)
 
+    @action(detail=False, methods=['get'])
+    def people_user_may_know(self, request, pk=None):
+        current_user = request.user
+        current_user_following_ids = current_user.following.values_list('following', flat=True)
+        users = User.objects.exclude(
+            id__in=current_user_following_ids).exclude(
+            is_staff__exact=True).exclude(
+            id__exact=12).order_by('?')[:5]
+
+        serializer = CustomUserSerializer(users, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
