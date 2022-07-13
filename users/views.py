4c54b95bfcdb075bc40e8c3b2310ffc6ff0b4421
adiglase <adiglase6@gmail.com>
2022-07-13 14:22:57 +0700

    Add search user api

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from multiprocessing import context
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -23,4 +24,11 @@ class CustomUserViewSet(viewsets.ModelViewSet):
             id__exact=12).order_by('?')[:5]
 
         serializer = CustomUserSerializer(users, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['post'])
+    def search(self, request):
+        user_identity = request.data.get('userIdentity')
+        filtered_user = User.objects.filter(Q(is_staff=False) & (Q(full_name__icontains=user_identity) | Q(username__icontains=user_identity)))[:5]
+        serializer = CustomUserSerializer(filtered_user, many=True, context={'request': request})
         return Response(serializer.data, status=status.HTTP_200_OK)
